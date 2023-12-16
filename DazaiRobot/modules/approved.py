53d5569759e51a69d2167ee6df51f2618b619d9d
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-12-16 20:25:36 +0530

    Create approved.py

---
+++ DazaiRobot/modules/approved.py
@@ -0,0 +1,16 @@
+import asyncio
+from DazaiRobot import pbot as Michiko
+from pyrogram import filters
+from pyrogram.types import ChatJoinRequest
+from pyrogram.errors import FloodWait
+
+@Michiko.on_chat_join_request(filters.group | filters.channel)
+async def approve(_, m: ChatJoinRequest):
+    if not m.from_user:
+        return
+    try:
+        await bot.approve_chat_join_request(m.chat.id, m.from_user.id)
+    except FloodWait as e:
+        print(f"Sleeping for {e.x + 2} seconds due to floodwaits!")
+        await asyncio.sleep(e.x + 2)
+        await _.approve_chat_join_request(m.chat.id, m.from_user.id)
