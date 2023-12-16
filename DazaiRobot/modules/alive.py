86dcbd1e625b026c3a81beb479f38a735803db8b
Shaurya <139495856+Shauryanoobhai@users.noreply.github.com>
2023-12-16 17:23:01 +0530

    Update alive.py

--- DazaiRobot/modules/alive.py
+++ DazaiRobot/modules/alive.py
@@ -13,14 +13,13 @@ VIDEO  = "https://te.legra.ph/file/f45343278d40b5017a206.mp4"
 async def awake(_, message: Message):
     TEXT = f"**ʜᴇʏ ʜᴏɴᴇʏ {message.from_user.mention},\n\nɪ ᴀᴍ {BOT_NAME}**\n━━━━━━━━━━━━━━━━━━━━━━\n\n"
     TEXT += f"» **ᴍʏ ʜᴏɴᴇʏ :** [Mikey](tg://user?id={OWNER_ID})\n\n"
-TEXT += f"» ᴍʏ  ꜱᴡᴇᴇᴛʜᴇᴀʀᴛ : [Devs](https://t.me/YumekoUpdate/9)\n\n"
     TEXT += f"» **ʟɪʙʀᴀʀʏ ᴠᴇʀsɪᴏɴ :** `{telever}` \n\n"
     TEXT += f"» **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{tlhver}` \n\n"
     TEXT += f"» **ᴘʏʀᴏɢʀᴀᴍ ᴠᴇʀsɪᴏɴ :** `{pyrover}` \n━━━━━━━━━━━━━━━━━━━━━\n\n"
     BUTTON = [
         [
             InlineKeyboardButton("ʜᴇʟᴘ", url=f"https://t.me/{BOT_USERNAME}?start=help"),
-            InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url=f"https://t.me/{SUPPORT_CHAT}"),
+            InlineKeyboardButton("ꜱᴡᴇᴇᴛʜᴇᴀʀᴛ", url=f"https://t.me/YumekoUpdate/9"),
         ]
     ]
     await message.reply_video(
