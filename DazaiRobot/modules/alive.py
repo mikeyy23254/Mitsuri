d8646d38346f2bc8dd4ca1632cc4f253da1ab94d
mikeyy23254 <157609132+mikeyy23254@users.noreply.github.com>
2024-02-18 23:42:50 +0530

    Update alive.py

--- DazaiRobot/modules/alive.py
+++ DazaiRobot/modules/alive.py
@@ -1,25 +1,25 @@
-from pyrogram import __version__ as pyrover
+from pyrogram import version as pyrover
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
-from telegram import __version__ as telever
-from telethon import __version__ as tlhver
+from telegram import version as telever
+from telethon import version as tlhver
 
 from DazaiRobot import BOT_NAME, BOT_USERNAME, OWNER_ID, START_IMG, SUPPORT_CHAT, pbot
 
-VIDEO  = "https://te.legra.ph/file/f45343278d40b5017a206.mp4"
+VIDEO  = "https://telegra.ph/file/13f087d75c66ca1759ef4.mp4"
 
 
 @pbot.on_message(filters.command("alive"))
 async def awake(_, message: Message):
-    TEXT = f"**ʜᴇʏ ʜᴏɴᴇʏ {message.from_user.mention},\n\nɪ ᴀᴍ {BOT_NAME}**\n━━━━━━━━━━━━━━━━━━━━━━\n\n"
-    TEXT += f"» **ᴍʏ ʜᴏɴᴇʏ :** [Mikey](tg://user?id={OWNER_ID})\n\n"
-    TEXT += f"» **ʟɪʙʀᴀʀʏ ᴠᴇʀsɪᴏɴ :** `{telever}` \n\n"
-    TEXT += f"» **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{tlhver}` \n\n"
-    TEXT += f"» **ᴘʏʀᴏɢʀᴀᴍ ᴠᴇʀsɪᴏɴ :** `{pyrover}` \n━━━━━━━━━━━━━━━━━━━━━\n\n"
+    TEXT = f"Hᴇʏ Hᴏɴᴇʏ {message.from_user.mention},\n\nɪ ᴀᴍ {BOT_NAME}\n━━━━━━━━━━━━━━━━━━━━━━\n\n"
+    TEXT += f"» ᴍʏ ʜᴏɴᴇʏ : [Mikey](tg://user?id={OWNER_ID})\n\n"
+    TEXT += f"» ʟɪʙʀᴀʀʏ ᴠᴇʀsɪᴏɴ : {telever} \n\n"
+    TEXT += f"» ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ : {tlhver} \n\n"
+    TEXT += f"» ᴘʏʀᴏɢʀᴀᴍ ᴠᴇʀsɪᴏɴ : {pyrover} \n━━━━━━━━━━━━━━━━━━━━━\n\n"
     BUTTON = [
         [
             InlineKeyboardButton("ʜᴇʟᴘ", url=f"https://t.me/{BOT_USERNAME}?start=help"),
-            InlineKeyboardButton("ꜱᴡᴇᴇᴛʜᴇᴀʀᴛ", url=f"https://t.me/YumekoUpdate/9"),
+            InlineKeyboardButton("Sᴜᴘᴘᴏʀᴛ", url=f"https://t.me/Ahjin_sprt"),
         ]
     ]
     await message.reply_video(
@@ -29,4 +29,4 @@ async def awake(_, message: Message):
     )
 
 
-__mod_name__ = "Aʟɪᴠᴇ"
+mod_name = "Aʟɪᴠᴇ"
