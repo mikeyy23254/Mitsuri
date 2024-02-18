f193c38a6caae52d42f023adec183b963ce16517
Shaurya (Levi) <139495856+Shauryanoobhai@users.noreply.github.com>
2024-02-19 03:52:17 +0530

    Update alive.py

--- DazaiRobot/modules/alive.py
+++ DazaiRobot/modules/alive.py
@@ -1,8 +1,8 @@
-from pyrogram import version as pyrover
+from pyrogram import __version__ as pyrover
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
-from telegram import version as telever
-from telethon import version as tlhver
+from telegram import __version__ as telever
+from telethon import __version__ as tlhver
 
 from DazaiRobot import BOT_NAME, BOT_USERNAME, OWNER_ID, START_IMG, SUPPORT_CHAT, pbot
 
@@ -11,11 +11,11 @@ VIDEO  = "https://telegra.ph/file/13f087d75c66ca1759ef4.mp4"
 
 @pbot.on_message(filters.command("alive"))
 async def awake(_, message: Message):
-    TEXT = f"Hᴇʏ Hᴏɴᴇʏ {message.from_user.mention},\n\nɪ ᴀᴍ {BOT_NAME}\n━━━━━━━━━━━━━━━━━━━━━━\n\n"
-    TEXT += f"» ᴍʏ ʜᴏɴᴇʏ : [Mikey](tg://user?id={OWNER_ID})\n\n"
-    TEXT += f"» ʟɪʙʀᴀʀʏ ᴠᴇʀsɪᴏɴ : {telever} \n\n"
-    TEXT += f"» ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ : {tlhver} \n\n"
-    TEXT += f"» ᴘʏʀᴏɢʀᴀᴍ ᴠᴇʀsɪᴏɴ : {pyrover} \n━━━━━━━━━━━━━━━━━━━━━\n\n"
+    TEXT = f"**Hᴇʏ Hᴏɴᴇʏ {message.from_user.mention},\n\nɪ ᴀᴍ {BOT_NAME}**\n━━━━━━━━━━━━━━━━━━━━━━\n\n"
+    TEXT += f"» **ᴍʏ ʜᴏɴᴇʏ:** [Mikey](tg://user?id={OWNER_ID})\n\n"
+    TEXT += f"» **ʟɪʙʀᴀʀʏ ᴠᴇʀsɪᴏɴ:** `{telever}` \n\n"
+    TEXT += f"» **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ:** `{tlhver}` \n\n"
+    TEXT += f"» **ᴘʏʀᴏɢʀᴀᴍ ᴠᴇʀsɪᴏɴ:** `{pyrover}| \n━━━━━━━━━━━━━━━━━━━━━\n\n"
     BUTTON = [
         [
             InlineKeyboardButton("ʜᴇʟᴘ", url=f"https://t.me/{BOT_USERNAME}?start=help"),
@@ -29,4 +29,4 @@ async def awake(_, message: Message):
     )
 
 
-mod_name = "Aʟɪᴠᴇ"
+__mod_name__ = "Aʟɪᴠᴇ"
