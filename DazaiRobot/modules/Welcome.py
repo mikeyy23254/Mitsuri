973508fba37b55eff388b03714439b138bb00a86
Shaurya (Levi) <139495856+Shauryanoobhai@users.noreply.github.com>
2024-04-21 18:15:34 +0530

    Update Welcome.py

--- DazaiRobot/modules/Welcome.py
+++ DazaiRobot/modules/Welcome.py
@@ -57,11 +57,11 @@ def welcomepic(pic, user, chat, id, uname):
     draw = ImageDraw.Draw(background)
     font = ImageFont.truetype('DazaiRobot/resources/default.ttf', size=40)
     welcome_font = ImageFont.truetype('DazaiRobot/resources/default.ttf', size=60)
-    draw.text((20, 100), f'NAME: {unidecode(user)}', fill=(100, 100, 100), font=font)
-    draw.text((30, 170), f'ID: {id}', fill=(100, 100, 100), font=font)
+    draw.text((20, 40), f'NAME: {unidecode(user)}', fill=(100, 100, 100), font=font)
+    draw.text((30, 70), f'ID: {id}', fill=(100, 100, 100), font=font)
     draw.text((30, 10), f"Welcome to {unidecode(chat)}", fill=(100, 100, 100), font=welcome_font)
-    draw.text((30,200), f"USERNAME : {uname}", fill=(100,100,100),font=font)
-    pfp_position = (300, 100)  
+    draw.text((30,100), f"USERNAME : {uname}", fill=(100,100,100),font=font)
+    pfp_position = (300, 120)  
     background.paste(pfp, pfp_position, pfp)  
     background.save(
         f"downloads/welcome#{id}.png"
