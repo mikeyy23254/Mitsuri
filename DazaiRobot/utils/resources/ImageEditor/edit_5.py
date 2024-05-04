7c77754ea28e1dde61d3e73157ef5f0124987a05
Shaurya(Levi) <139495856+Shauryanoobhai@users.noreply.github.com>
2024-05-04 12:03:20 +0530

    Update edit_5.py

--- DazaiRobot/utils/resources/ImageEditor/edit_5.py
+++ DazaiRobot/utils/resources/ImageEditor/edit_5.py
@@ -3,7 +3,7 @@ import os
 import shutil
 
 from DazaiRobot import LOGGER
-from aiohttp import Client
+from DazaiRobot import pbot as Client 
 
 async def normalglitch_1(client: Client, message: Message):
     try:
