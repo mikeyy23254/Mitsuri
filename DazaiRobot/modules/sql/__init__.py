f44bd621003fdcde6f94dc685e7ad41d4077b2ae
Obanai <157609132+mikeyy23254@users.noreply.github.com>
2024-05-18 12:13:58 +0530

    Update __init__.py

--- DazaiRobot/modules/sql/__init__.py
+++ DazaiRobot/modules/sql/__init__.py
@@ -5,23 +5,29 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 from DazaiRobot import DB_URI
 from DazaiRobot import LOGGER as log
 
+# Create a base class for the ORM models
+BASE = declarative_base()
+
+# Check and modify DB_URI if necessary
 if DB_URI and DB_URI.startswith("postgres://"):
     DB_URI = DB_URI.replace("postgres://", "postgresql://", 1)
 
-
+# Function to start the database session
 def start() -> scoped_session:
+    if not DB_URI:
+        log.error("DB_URI is not set or is None")
+        raise ValueError("DB_URI is not set or is None")
+
     engine = create_engine(DB_URI, client_encoding="utf8")
     log.info("[PostgreSQL] Connecting to database......")
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
     return scoped_session(sessionmaker(bind=engine, autoflush=False))
 
-
-BASE = declarative_base()
 try:
+    # Initialize the session
     SESSION = start()
+    log.info("[PostgreSQL] Connection successful, session started.")
 except Exception as e:
     log.exception(f"[PostgreSQL] Failed to connect due to {e}")
-    exit()
-
-log.info("[PostgreSQL] Connection successful, session started.")
+    raise
